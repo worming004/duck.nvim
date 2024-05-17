0d12f82b52fab088520110ce02babf35722fc102
Mathieu Scolas <worming@hotmail.be>
2024-05-18 01:18:29 +0200

    code style fix

--- lua/duck.lua
+++ lua/duck.lua
@@ -7,63 +7,63 @@ local random_wandle = require('random_waddle')
 local conf = { character = "🦆", speed = 10, width = 2, height = 1, color = "none", blend = 100 }
 
 local default_strategies = {
-    always_right_strategy = function(positions)
-        return { col = positions.col + 1, row = positions.row }
-    end,
-    random_waddle = function(positions)
-        return random_wandle:random_waddle()(positions)
-    end
+  always_right_strategy = function(positions)
+    return { col = positions.col + 1, row = positions.row }
+  end,
+  random_waddle = function(positions)
+    return random_wandle:random_waddle()(positions)
+  end
 }
 
 
 M.default_strategies = default_strategies
 
 M.hatch = function(character, speed, color, strategy)
-    local buf = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_lines(buf, 0, 1, true, { character or conf.character })
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buf, 0, 1, true, { character or conf.character })
 
-    local duck = vim.api.nvim_open_win(buf, false, {
-        relative = 'cursor', style = 'minimal', row = 1, col = 1, width = conf.width, height = conf.height
-    })
-    vim.cmd("hi Duck" .. duck .. " guifg=" .. (color or conf.color) .. " guibg=none blend=" .. conf.blend)
-    vim.api.nvim_win_set_option(duck, 'winhighlight', 'Normal:Duck' .. duck)
+  local duck = vim.api.nvim_open_win(buf, false, {
+    relative = 'cursor', style = 'minimal', row = 1, col = 1, width = conf.width, height = conf.height
+  })
+  vim.cmd("hi Duck" .. duck .. " guifg=" .. (color or conf.color) .. " guibg=none blend=" .. conf.blend)
+  vim.api.nvim_win_set_option(duck, 'winhighlight', 'Normal:Duck' .. duck)
 
-    if strategy == nil then
-        strategy = default_strategies.random_waddle
-    end
+  if strategy == nil then
+    strategy = default_strategies.random_waddle
+  end
 
-    waddle.waddle(duck, speed, conf, strategy)
+  waddle.waddle(duck, speed, conf, strategy)
 end
 
 M.cook = function()
-    local last_duck = M.ducks_list[#M.ducks_list]
+  local last_duck = M.ducks_list[#M.ducks_list]
 
-    if not last_duck then
-        vim.notify("No ducks to cook!")
-        return
-    end
+  if not last_duck then
+    vim.notify("No ducks to cook!")
+    return
+  end
 
-    local duck = last_duck['name']
-    local timer = last_duck['timer']
-    table.remove(M.ducks_list, #M.ducks_list)
-    timer:stop()
+  local duck = last_duck['name']
+  local timer = last_duck['timer']
+  table.remove(M.ducks_list, #M.ducks_list)
+  timer:stop()
 
-    vim.api.nvim_win_close(duck, true)
+  vim.api.nvim_win_close(duck, true)
 end
 
 M.cook_all = function()
-    if #M.ducks_list <= 0 then
-        vim.notify("No ducks to cook!")
-        return
-    end
+  if #M.ducks_list <= 0 then
+    vim.notify("No ducks to cook!")
+    return
+  end
 
-    while (#M.ducks_list > 0) do
-        M.cook()
-    end
+  while (#M.ducks_list > 0) do
+    M.cook()
+  end
 end
 
 M.setup = function(opts)
-    conf = vim.tbl_deep_extend('force', conf, opts or {})
+  conf = vim.tbl_deep_extend('force', conf, opts or {})
 end
 
 return M
--- lua/waddle.lua
+++ lua/waddle.lua
@@ -2,28 +2,28 @@ local M = {}
 M.ducks_list = {}
 
 M.waddle = function(buf, speed, conf, strategy)
-    local timer = vim.loop.new_timer()
-    local new_duck = { name = buf, timer = timer }
-    table.insert(M.ducks_list, new_duck)
+  local timer = vim.loop.new_timer()
+  local new_duck = { name = buf, timer = timer }
+  table.insert(M.ducks_list, new_duck)
 
-    local waddle_period = 1000 / (speed or conf.speed)
-    vim.loop.timer_start(timer, 1000, waddle_period, vim.schedule_wrap(function()
-        if vim.api.nvim_win_is_valid(buf) then
-            local config = vim.api.nvim_win_get_config(buf)
-            local col, row = 0, 0
-            if vim.version().minor < 10 then -- Neovim 0.9
-                col, row = config["col"][false], config["row"][false]
-            else                       -- Neovim 0.10
-                col, row = config["col"], config["row"]
-            end
+  local waddle_period = 1000 / (speed or conf.speed)
+  vim.loop.timer_start(timer, 1000, waddle_period, vim.schedule_wrap(function()
+    if vim.api.nvim_win_is_valid(buf) then
+      local config = vim.api.nvim_win_get_config(buf)
+      local col, row = 0, 0
+      if vim.version().minor < 10 then       -- Neovim 0.9
+        col, row = config["col"][false], config["row"][false]
+      else                                   -- Neovim 0.10
+        col, row = config["col"], config["row"]
+      end
 
-            local pos = strategy({ row = row, col = col })
-            config["row"] = pos.row
-            config["col"] = pos.col
+      local pos = strategy({ row = row, col = col })
+      config["row"] = pos.row
+      config["col"] = pos.col
 
-            vim.api.nvim_win_set_config(buf, config)
-        end
-    end))
+      vim.api.nvim_win_set_config(buf, config)
+    end
+  end))
 end
 
 return M
